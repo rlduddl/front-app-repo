4ceef97c1e7baa554529de2f5233a6accd85b973
rlduddl <qkrrldud5519@gmail.com>
2026-06-01 07:50:18 +0000

    v4

--- src/App.js
+++ src/App.js
@@ -36,7 +36,7 @@ function App(props) {
             <CloudIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1, textAlign: 'center' }}>
-            EKS DEMO Blog v3
+            EKS DEMO Blog v10 commit plz
           </Typography>
           {new Date().toLocaleTimeString()}
         </Toolbar>
